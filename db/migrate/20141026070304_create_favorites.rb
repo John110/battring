28e3fdaf8b961faaa05f97d5dbb4b17164047482
John110 <monaeru0612@hotmail.co.jp>
2014-11-19 15:29:26 +0900

    不足していたマイグレーションファイルをアップ

---
+++ db/migrate/20141026070304_create_favorites.rb
@@ -0,0 +1,11 @@
+class CreateFavorites < ActiveRecord::Migration
+  def change
+    create_table :favorites do |t|
+      t.string :title
+      t.references :user, index: true
+      t.references :project, index: true
+
+      t.timestamps
+    end
+  end
+end
